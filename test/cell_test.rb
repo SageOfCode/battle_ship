c48b7da58fe98127adf9020b31080ea6bb93174f
Sage Gonzales <sagegonzales15@gmail.com>
2020-08-25 08:27:34 -0600

    passes placed and empty tests

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -5,8 +5,25 @@ require 'Minitest/pride'
 
 class CellTest < Minitest::Test
 
-  def test_that_it_exists
+  def test_that_it_exists_and_has_attributes
     cell = Cell.new("B4")
-    assert_instance_of Cell, cell 
+    assert_instance_of Cell, cell
+    assert_equal "B4", cell.coordinate
+    assert_nil cell.ship
+  end
+
+  def test_it_knows_if_empty
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    assert_equal true, cell.empty?
+    cell.place_ship(cruiser)
+    assert_equal false, cell.empty?
+  end
+
+  def test_ship_can_be_placed
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    assert_equal cruiser, cell.ship
   end
 end
