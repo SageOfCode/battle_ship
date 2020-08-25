673c28d550bde3cb353806f4107c412a2a0c3a0b
Shaun James <ShaunDaneJames@Gmail.com>
2020-08-25 17:27:27 -0600

    Adds and passes board knows about cells

--- test/board_test.rb
+++ test/board_test.rb
@@ -10,4 +10,11 @@ class BoardTest < Minitest::Test
     board = Board.new
     assert_instance_of Board, board
   end
+
+  def test_it_knows_about_cells
+    board = Board.new
+    assert_equal Hash, board.cells.class
+    assert_equal 16, board.cells.keys.count
+    assert_instance_of Cell, board.cells["A1"]
+  end
 end
