55373f000a18ea2b08a032bc8f3f7644a25cb0b2
Sage Gonzales <sagegonzales15@gmail.com>
2020-08-28 20:05:00 -0600

    adds runner file

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,4 +5,9 @@ class Turn
     @player = player
     @computer = computer
   end
+
+  def main_menu
+    puts "Welcome to BATTLESHIP /nEnter p to play. Enter q to quit."
+
+  end
 end
---
+++ runner.rb
@@ -0,0 +1,10 @@
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/player'
+require './lib/computer'
+require './lib/turn'
+
+game = Turn.new
+
+game.main_menu
